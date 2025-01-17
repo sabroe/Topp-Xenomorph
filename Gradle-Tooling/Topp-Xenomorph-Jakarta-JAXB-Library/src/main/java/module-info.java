dc92a988dcb0c10a45c0909f64f0f923c4c81027
Morten Sabroe Mortensen <morten.sabroe.mortensen@beumer.com>
2025-01-17 11:10:56 +0100

    Modified dependencies.

--- Gradle-Tooling/Topp-Xenomorph-Jakarta-JAXB-Library/src/main/java/module-info.java
+++ Gradle-Tooling/Topp-Xenomorph-Jakarta-JAXB-Library/src/main/java/module-info.java
@@ -3,16 +3,13 @@
  */
 module com.yelstream.topp.jakarta.jaxb {
     requires static lombok;
-
-/*
     requires jakarta.annotation;
     requires jakarta.activation;
     requires com.sun.activation.registries;
     requires jakarta.xml.bind;
     requires com.sun.xml.bind.core;
     requires com.sun.xml.bind;
-//    requires com.sun.tools.jxc;
-*/
-
-//    exports urn.iso.std.iso._20022.tech.pain_001_001_02;
+    requires com.sun.tools.jxc;
+    requires com.sun.tools.xjc;
+    exports com.yelstream.topp.jakarta;
 }
